96452e12341f00914817544be890a3c02e7995bf
minrazzz <minraz123@gmail.com>
2023-08-25 01:16:17 +0545

    navbar created

---
+++ ecommerce/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: [
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+   ],
+   theme: {
+      extend: {
+         backgroundImage: {
+            "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+            "gradient-conic":
+               "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+         },
+      },
+   },
+   plugins: [],
+};
+
+//if you want to override all the media queries of the tailwind then add the screen inside theme
+//if you just want to override single media quaries eg sm or md then add the screen inside extend as shown above
